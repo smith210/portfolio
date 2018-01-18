65640006b7281fb967d4f28522f15081caa17271
smith210 <smith210@wwu.edu>
2018-01-18 12:44:40 -0800

    adding HelloWorld.java

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+//Name: Martin Smith
+//CSCI 241 - Winter 2018
+//Lab1 - HelloWorld.java
+
+public class HelloWorld {
+  public static void main(String [] args){
+
+    System.out.println("Hello, World!!")
+
+  }
+
+
+}
