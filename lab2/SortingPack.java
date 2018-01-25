baeb242b2c69682f208b9fd0a93154c0300341c0
smith210 <smith210@wwu.edu>
2018-01-25 12:08:51 -0800

    Skeleton of SortingPack.java

---
+++ lab2/SortingPack.java
@@ -0,0 +1,14 @@
+//Name : Martin Smith
+//CSCI 241 - Winter 2018
+//Date : January 25th 2018
+//File : SortingPack.java
+
+public class SortingPack {
+
+//main method
+  public static void main (String[]args){
+
+
+  }
+
+}
