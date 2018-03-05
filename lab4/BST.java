e9163f7831ec7d70d28187724a8d007125d682a1
smith210 <smith210@wwu.edu>
2018-03-04 18:56:49 -0800

    fixed errors in insert and delete, created helper functions for those two methods

--- lab4/BST.java
+++ lab4/BST.java
@@ -11,81 +11,87 @@ public class BST{
   }
 
   public boolean search(int target){
+      //create a pointer value
       Node curr = root;
-
+      //if there is no node, return false (target will not be there)
       if(curr == null){
         return false;
       }else{
-        while(curr.left != null && curr.right != null){
-          int nodeCheck = curr.key;
+        //loop through the tree until can't no more
+        while(curr != null){
+            int nodeCheck = curr.key;
+            //check to see if the key is equal to the target
+            //if it is, return true
+            if (nodeCheck == target){
+              System.out.println("VALUE IN BST EXISTS");
+              return true;
+            }
+            //if its not, go through the tree in repsective direction
 
-          if(nodeCheck == target){
-            return true;
-          }else{
-            if(nodeCheck > target){
+            //Go right if the target is bigger than current key
+            if (nodeCheck < target){
                 curr = curr.right;
+            //Go left if the target is smaller than the current key
             }else{
                 curr = curr.left;
             }
-          }
         }
+        //if out of the loop, return false
+        System.out.println("NO SUCH VALUE EXISTS");
         return false;
       }
   }
 
-  public void insert(int target){
-      Node curr = root;
+    public void insert(int target){
+      //update values of root (w/ help from insertHelp)
+      root = insertHelp(root, null, target);
 
-      if(curr == null){
-        root = new Node(target);
-        return;
-      }else{
-        Node temp = curr;
-        while(curr.left != null && curr.right != null){
-          int nodeCheck = curr.key;
+    }
 
-          if(nodeCheck == target){
-            curr.key = target;
-          }else{
-            if(nodeCheck > target){
-                temp = curr;
-                curr = curr.right;
-            }else{
-                temp = curr;
-                curr = curr.left;
-            }
-          }
+    public Node insertHelp(Node node, Node guardian, int target){
+
+        //if the node starts at as null, create new node and return.
+        if(node == null){
+          node = new Node(target);
+          return node;
         }
 
-        curr.key = target;
-        curr.parent = temp;
-        System.out.print(curr.parent.key);
-      }
+        int nodeCheck = node.key;
+        //if target value is equal to the key value, return node
+        if(nodeCheck == target){
+            return node;
+        //if target value is bigger, go through right subtree
+        }else if (nodeCheck < target){
 
-  }
+          System.out.println("GOING RIGHT");
+          //parse through the right subtree of node
+          node.right = insertHelp(node.right, node, target);
+          //set parent value of node to the guardian
+          node.parent = guardian;
+        //if target value is smaller, go through left subtree
+        }else{
+          System.out.println("GOING LEFT");
+          //parse through the left subtree of node
+          node.left = insertHelp(node.left, node, target);
+          //set parent value of node to the guardian
+          node.parent = guardian;
+        }
+
+
+        return node;
+    }
+
+
 
   public void delete(int target){
-    Node curr = root;
 
     boolean foundTarget = search(target);
     //make sure the target being deleted is in the tree
     if(foundTarget){
       //get the node up to date
-      while(curr.left != null && curr.right != null){
-        int nodeCheck = curr.key;
-
-        if(nodeCheck == target){
-          break;
-        }else{
-          if(nodeCheck > target){
-              curr = curr.right;
-          }else{
-              curr = curr.left;
-          }
-        }
-      }
+      root = deleteHelper(root, target);
       //case 1: if the node being deleted has no children
-      if (curr.left == null && curr.right == null){
+      /*if (curr.left == null && curr.right == null){
         curr = null;
       //case 2.1: if the node has only one child/one subtree -left
       } else if (curr.right == null){
@@ -110,22 +116,101 @@ public class BST{
         //set the smallest value equal to where curr is
         curr.key = smallestKey;
 
-      }
+      }*/
 
     }
 
   }
 
-  public void traverse(){
-      Node curr = root;
+  public Node deleteHelper(Node node, int target){
+      int nodeCheck = node.key;
+      if (nodeCheck < target){
+          System.out.println("GRIM REAPER GOING RIGHT");
+          node.right = deleteHelper(node.right, target);
+          return node;
+      }else if (nodeCheck > target){
+          System.out.println("GRIM REAPER GOING LEFT");
+          node.left = deleteHelper(node.left, target);
+          return node;
+      }else{
+        //case 1: if the node being deleted has no children
+        if (node.left == null && node.right == null){
+          System.out.println("NODE HAS NO CHILDREN TO PASS ITS LEGACY TO");
+          node = null;
+          return node;
+        //case 2.1: if the node has only one child/one subtree -left
+        }else if (node.right == null){
+          System.out.println("NODE HAS A LEFT CHILD TO PASS ITS LEGACY TO");
+          node = childCustody(node, node.left);
+          return node;
+        //case 2.2: if the node has only one child/one subtree - right
+        }else if (node.left == null){
+          System.out.println("NODE HAS A RIGHT CHILD TO PASS ITS LEGACY TO");
+          node = childCustody(node, node.right);
+          return node;
+        //case 3: if the node has both children/both subtrees
+        }else{
+          System.out.println("NODE HAS BOTH CHILDREN TO PASS ITS LEGACY TO");
+          //find the smallest node on the right side of the family
+          int smallest = findSmallest(node);
+          //delete the spot where the smallest node originally was
+          node = deleteHelper(node, smallest);
+          //complete transaction by replacing the node value with smallest
+          node.key = smallest;
+          //make sure the new guardian is known by the children
+          System.out.println(node.right.parent.key);
+          System.out.println(node.left.parent.key);
+          return node;
+        }
+      }
+  }
 
-      actuallyTraverse(curr);
+  public Node childCustody(Node deceased, Node child){
+      System.out.println(deceased.parent.key);
+
+      //if the deceased has no parents, replace deceased with its child
+      if (deceased.parent == null){
+          deceased = child;
+          return deceased;
+      //if the deceased is parents right child
+      } else if(deceased.parent.right == deceased){
+          //give custody of child to the parent of the deceased
+          System.out.println("RIGHT PARENT HAS CONFERRED WITH CHILD");
+          deceased.parent.right = child;
+          child.parent = deceased.parent;
+          //replace deceased with the living child
+          deceased = child;
+          System.out.println("THE CHILD HAS REPLACED THE DECEASED");
+          return deceased;
+      //if the deceased is parents left child
+      } else {
+          //give custody of child to the parent of the deceased
+          System.out.println("LEFT PARENT HAS CONFERRED WITH CHILD");
+          deceased.parent.left = child;
+          child.parent = deceased.parent;
+          //replace deceased with the living child
+          deceased = child;
+          System.out.println("THE CHILD HAS REPLACED THE DECEASED");
+          return deceased;
+      }
+
+  }
+
+  public int findSmallest(Node node){
+      Node journeyForSmall = node.right;
+      while(journeyForSmall.left != null){
+        journeyForSmall = journeyForSmall.left;
+      }
+      return journeyForSmall.key;
+
+  }
+
+  public void traverse(){
+      actuallyTraverse(root);
   }
 
   public void actuallyTraverse(Node node){
-      if(node == null){
-        System.out.print("");
-      }else{
+      if(node != null){
         actuallyTraverse(node.left);
         System.out.print(node.key + " ");
         actuallyTraverse(node.right);
